7d851ea28ece0666803ac6639a5ed74565687a81
Elias Ortega <eliasortega2011@gmail.com>
2023-05-02 14:54:23 -0400

    script.js funcion addEventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
+var obtenerCodigo = document.querySelector('#obtcodigo');
+    var checkbox = document.querySelector('#checkUno');
 
+    function ValidarDescuento(){
+      obtenerCodigo.addEventListener('click', function() {
+        if (checkbox.checked) {
+          var codigoDescuento = ['MNO123ST', 'PQR340XS', 'GUP770LM'];
+          var codigoAleatorio = codigoDescuento[Math.floor(Math.random() * codigoDescuento.length)];
+          document.querySelector('#codigo').textContent = codigoAleatorio;
+        } else {
+          alert('Debe aceptar los términos y condiciones');
+        }
+      });
+      
+      checkbox.addEventListener('click', function() {
+        if (checkbox.checked) {
+          document.querySelector('#codigo').textContent = '';
+        }
+      });  
+
+    }
+    
+    ValidarDescuento();
  
 
     
